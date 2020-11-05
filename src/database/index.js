4c32b2a2ec4713f06ba2b4072692546a85746ef0
Rosa, Genesis <mrgenesis@hotmail.com>
2020-11-05 14:07:39 -0400

    definido useCreateIndex e useUnifiedTopology para true para remover avisor do console

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/noderest', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1/noderest', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 mongoose.Promise = global.Promise;
 
