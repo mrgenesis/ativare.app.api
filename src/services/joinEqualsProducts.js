0b30c7665f878349531fd17b8eed238fa4817a24
Rosa, Genesis <mrgenesis@hotmail.com>
2020-12-11 13:42:15 -0400

    removido um console.log do codigo

--- src/services/joinEqualsProducts.js
+++ src/services/joinEqualsProducts.js
@@ -1,24 +1,23 @@
-function joinEqualsProducts (products = '') {
+function joinEqualsProducts(products = '') {
   let joinedProducts = {}
-  
-  // Set a variable will storage the results
-  , joinedProductsTheKeyAreItsIds = {}
-  , joinedProductsId = []
-  , joinedProductsObj = [];
-  
-  if (products === '') 
-  throw new Error('Don\'t sent a object');
-  
+
+    // Set a variable will storage the results
+    , joinedProductsTheKeyAreItsIds = {}
+    , joinedProductsId = []
+    , joinedProductsObj = [];
+
+  if (products === '')
+    throw new Error('Don\'t sent a object');
+
   // The "products" variable is the result of "findById" of mongoose
   for (let product of products) {
     if (!product._id)
-      throw new Error ('Product of list without property _id');
-    
+      throw new Error('Product of list without property _id');
+
     if (!product.amount)
       throw new Error('Product of list without property amount');
 
     if (!Number.isInteger(product.amount)) {
-      console.log('Number.isInteger:', product.amount)
       throw new Error('The property "amount" should be number');
     }
 
