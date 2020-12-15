9acc3c2e4516db180a7e72c8a465ad8af7a75275
Rosa, Genesis <mrgenesis@hotmail.com>
2020-12-15 08:38:57 -0400

    modificado o codigo para funcionar com auxilio do arquivo budgetCalc que gera os o total e os dados privados na propriedade privateDetail do orcamento

--- src/app/controllers/budgetController.js
+++ src/app/controllers/budgetController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
-const productRules = require('../../services/productRules');
+const budgetCalc = require('../../services/budgetCalc');
 
 const router = express.Router();
 
@@ -14,57 +14,32 @@ router.get('/:budgetId', async (req, res) => {
   try {
     let budget = await Budget.findById(budgetId);
     let selectMaterialsAll = await Material.find({});
+    let { total, privateDetail } = budgetCalc(budget._doc.productsList, selectMaterialsAll);
     budget = budget._doc;
 
-    budget['total'] = 0;
-    budget['privateDetail'] = {
-      products: {}
-    };
-    
-    let joinedProducts = {};
-    // Junta item iguais e soma os valores da propriedade 'amount'
-    for (const product of Object.values(budget.productsList)) { // Contagem dos itens iguais
-        if (joinedProducts[product._id]) { // id já existe, somar amount
-            joinedProducts[product._id].amount += product.amount;
-        } else { // id não existe, criar o objeto com o amount inicial
-            joinedProducts[product._id] = { ...product };
-        }
-    } 
-    
-    // Add preços unitários nos produtos da lista
-    // Add a quantidade de cada material de acordo com o montante selecionado
-    for (const k in joinedProducts) { //o valor de 'k' será algo parecido com '5fcebebaef72e11a9a790335'. É a _id do produto
-      joinedProducts[k].materials.map((materialsOfProduct, index) => { // 'materialsOfProduct' é exatamente igual (===) a 'joinedProducts[k].materials[index]'.
-        selectMaterialsAll.find(obj => {
-          if(obj._id == materialsOfProduct._id) {
-            joinedProducts[k].materials[index] = { ...joinedProducts[k].materials[index], unitPrice: obj.unitPrice }; // Add preço
-            joinedProducts[k].materials[index].configAmount = productRules[joinedProducts[k].materials[index].rule](joinedProducts[k].amount); // cofigura quantidade usando "rule"
-            joinedProducts[k].materials[index].subTotal = joinedProducts[k].materials[index].configAmount * joinedProducts[k].materials[index].unitPrice; // Calc o subtotal
-            budget['total'] += joinedProducts[k].materials[index].subTotal;
-          }
-        });
-      });
-    } 
+    console.log('total: ' + total, privateDetail)
+
+    budget['total'] = total;
+    budget['privateDetail'] = [];
 
     if (true) { // TODO: Colocar restrição para executar apenas em logins com perfil admin
-      budget.privateDetail.products = Object.values(joinedProducts);
+      budget.privateDetail = privateDetail;
     }
 
     res.status(200).send({
       ...budget
     });
 
-  } catch (err){
+  } catch (err) {
     res.status(500).send({
       error: 'Não consegui processar esta solicitação.'
     });
     console.error(err);
   }
 
-  
 });
 router.post('/create', async (req, res) => {
-  
+
   try {
     const budget = await Budget.create({
       ...req.body,
