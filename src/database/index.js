ef42ff8196beaa6e101d1f2e4debc0a01c9c7cbd
Rosa, Genesis <mrgenesis@hotmail.com>
2020-12-18 13:20:56 -0400

    a propriedade useFindAndModify foi definida para false conforme orientado no aviso no log

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
-    , dataConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgjg9.azure.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  , dataConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgjg9.azure.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-
+// a propriedade useFindAndModify foi definida para false devido a atualização ocorrida no drive do Mongo
+// doc: https://mongoosejs.com/docs/deprecations.html#findandmodify
 mongoose.connect(dataConnection, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 mongoose.Promise = global.Promise;
 
