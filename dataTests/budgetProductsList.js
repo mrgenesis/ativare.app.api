c00603b052a20e3953ed00bd55712369f553e2d6
Rosa, Genesis <mrgenesis@hotmail.com>
2020-12-23 14:23:53 -0400

    adicionado arquivo modelo para execução de testes

---
+++ dataTests/budgetProductsList.js
@@ -0,0 +1,421 @@
+const budgetProductsList = [
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok",
+        "unitPrice": 100,
+        "configAmount": 15,
+        "subTotal": 1500
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "3° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a1xsdret84bd693b",
+    "name": "XXXXXXXXX okay",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a1xsdret84bd693b",
+    "name": "XXXXXXXXX okay",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a1xsdret84bd693b",
+    "name": "XXXXXXXXX okay",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a1xsdret84bd693b",
+    "name": "XXXXXXXXX okay",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "147dc23001b82693dfsagr",
+    "name": "Novo Nome",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "3° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "3° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "147dc23001b82693dfsagr",
+    "name": "Novo Nome",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "3° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "147dc23001b82693dfsagr",
+    "name": "Novo Nome",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "5fd2a9a147dc23001b82693b",
+    "name": "pro ok",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "2° Pavimento",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  },
+  {
+    "materials": [
+      {
+        "_id": "5fd2a97547dc23001b82693a",
+        "rule": "amountsAreEquals",
+        "name": "Ok"
+      }
+    ],
+    "_id": "147dc23001b82693dfsagr",
+    "name": "Novo Nome",
+    "category": "Automação",
+    "group": "Iluminação",
+    "description": "uma desc.",
+    "__v": 0,
+    "floor": "Térreo",
+    "amount": 5,
+    "homeLocationName": "Sala"
+  }
+];
+
+const materialsList = [
+  {
+    "_id": "5fd2a97547dc23001b82693a",
+    "name": "Ok",
+    "unitPrice": 100,
+    "rule": "amountsAreEquals",
+    "__v": 0
+  },
+  {
+    "_id": "5fd38dfa47dc23001b82693d",
+    "name": "Mini Dimmer",
+    "unitPrice": 620,
+    "rule": "oneIndividualItemForFuorSelected",
+    "__v": 0
+  }
+];
+
+const oneItemWithoutProperty_id = JSON.parse(JSON.stringify(budgetProductsList));
+delete oneItemWithoutProperty_id[0]._id;
+
+const oneItemWithoutPropertyAmount = JSON.parse(JSON.stringify(budgetProductsList));
+delete oneItemWithoutPropertyAmount[0].amount;
+
+const propertyAmountIsNotNumber = JSON.parse(JSON.stringify(budgetProductsList));
+propertyAmountIsNotNumber[0].amount = 'xxx'; // strin
+
+const oneItemWithoutFloorProperty = JSON.parse(JSON.stringify(budgetProductsList));
+delete oneItemWithoutFloorProperty[0].floor;
+
+module.exports = {
+  budgetProductsList,
+  oneItemWithoutProperty_id,
+  oneItemWithoutPropertyAmount,
+  propertyAmountIsNotNumber,
+  oneItemWithoutFloorProperty,
+  materialsList
+};
