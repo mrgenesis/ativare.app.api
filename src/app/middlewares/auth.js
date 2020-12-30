a3dd8a05b50c2a3dc33cb72ee47aef05ed8d0fe0
Rosa, Genesis <mrgenesis@hotmail.com>
2020-12-30 08:22:30 -0400

    definido resposta de erro de 400 para 401 e adicionado mais uma regra de validação de token

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,25 +2,27 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../../config/auth-config');
 
 module.exports = (req, res, next) => {
-    console.log(req.headers)
-    const authHeader = req.headers.authorization;
-    if (!authHeader)
-        return res.status(401).send({ Error: "No token provaded" });
+  const authHeader = req.headers.authorization;
+  if (!authHeader)
+    return res.status(401).send({ Error: "No token provaded" });
 
-    const parts = authHeader.split(' ');
-    if (!parts.length === 2)
-        return res.status(401).send({ Erro: "Token error"});
+  const parts = authHeader.split(' ');
+  if (!parts.length === 2)
+    return res.status(401).send({ Erro: "Token error" });
 
-    const [ schema, token ] =  parts;
-    if(!/^Bearer$/i.test(schema))
-        return res.status(401).send({ Erro: "Token malformatted"});
+  const [schema, token] = parts;
+  if (!/^Bearer$/i.test(schema))
+    return res.status(401).send({ Erro: "Token malformatted" });
 
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
-        if(err) return res.status(401).send({ Error: "Token invalid" });
+  if (token === null || token === 'null')
+    return res.status(401).send({ Error: "Token invalid" });
 
-        req.userId = decoded.id;
-        return next();
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) return res.status(401).send({ Error: "Token invalid" });
 
-    });
-    
+    req.userId = decoded.id;
+    return next();
+
+  });
+
 };
