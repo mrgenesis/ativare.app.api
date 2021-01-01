0a4b554a34cf5096565963cfc5240509a2ef974a
Rosa, Genesis <mrgenesis@hotmail.com>
2021-01-01 09:41:56 -0400

    criada a rota /edit para editar produtos

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -33,10 +33,32 @@ router.get('/automation', async (req, res) => {
   });
   res.status(200).send([...automationItems]);
 });
+router.post('/edit', async (req, res) => {
+  const productUpdate = req.body;
+  try {
+    if (!productUpdate.code) {
+      throw new Error('A propriedade code não foi informada.');
+    }
+    const codeProperty = { code: productUpdate.code };
+    const set = { '$set': { name: productUpdate.name, description: productUpdate.description } }
+    const getNew = { new: true };
+    const product = await Product.findOneAndUpdate(codeProperty, set, getNew);
+
+    if (!product) {
+      res.status(204).send({});
+    }
+    return res.status(201).send({
+      ...product.toObject()
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(400).send({ Error: 'Não foi possível processar esta solicitação' })
+  }
+});
 
 router.get('/:productId', async (req, res) => {
   const { productId } = req.params;
-  console.log(productId)
   try {
     const product = await Product.findOne({ code: productId });
     res.status(200).send({
