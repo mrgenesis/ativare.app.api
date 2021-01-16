d989a6f71bd9e3554d3d6abb3fbd3ff0328ec1ed
Rosa, Genesis <mrgenesis@hotmail.com>
2021-01-16 10:42:04 -0400

    arquivos deletados: as funções deles foram movidas para a pasta helpers

--- src/app/middlewares/isAdmin.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (req, res, next) => {
-  const perfilCurrentUser = req.app.locals.auth.perfilType;
-  req.app.locals.isAdmin = (perfilCurrentUser.indexOf('admin') !== -1);
-  return next();
-}
